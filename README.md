fe656be04b3ecd6571b3ba3722d56bf146a9b946
hugh-braico <15304925+hugh-braico@users.noreply.github.com>
2022-02-11 11:19:37 +1100

    Support importing human-readable timestamps

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Converts [tunawithbacon](http://tunawithbacon.com/)-formatted csv files to times
 ## How to use
 
 Download the release exe from the [releases page](https://github.com/hugh-braico/twb-to-yt-timestamp/releases/), then click-and-drag TWB-formatted csv files onto it.
+
+## Building your own release exe
+
+```bash
+pyinstaller --onefile -i bigband.ico csv_to_stamp.py
+```
--- csv_to_stamp.py
+++ csv_to_stamp.py
@@ -4,19 +4,48 @@ import re
 from pyperclip import copy
 
 
-def seconds_to_timestamp(total_seconds: int) -> str:
+def total_seconds_to_timestamp(total_seconds: int) -> str:
     hours = total_seconds // 3600
     minutes = (total_seconds - hours*3600) // 60
     seconds = total_seconds - hours*3600 - minutes*60
     return f"{hours:01d}:{minutes:02d}:{seconds:02d}"
 
 
+def hms_to_timestamp(h: int, m: int, s: int) -> str:
+    return f"{h:01d}:{m:02d}:{s:02d}"
+
+
 def url_to_timestamp(url: str) -> str:
-    seconds_match = re.search(r't=([0-9]+)', url)
-    if seconds_match:
-        return seconds_to_timestamp(int(seconds_match.group(1)))
+    timestamp_match = re.search(r'[?&]t=([0-9hms]+)', url)
+
+    # If there is no timestamp then it's probably just the start of the video
+    if not timestamp_match: 
+        return "0:00:00"
+
+    timestamp = timestamp_match.group(1)
+
+    # If this is a simple timestamp with total seconds eg. t=12345, do that
+    total_seconds_match = re.fullmatch(r'([0-9]+)', timestamp)
+    if total_seconds_match:
+        return total_seconds_to_timestamp(int(total_seconds_match.group(1)))
+
+    # If the timestamp has h/m/s fields, extract them
+    h_match = re.search(r'([0-9]+)h', timestamp)
+    s_match = re.search(r'([0-9]+)s', timestamp)
+    m_match = re.search(r'([0-9]+)m', timestamp)
+    if h_match:
+        h = int(h_match.group(1))
     else:
-        return seconds_to_timestamp(0) 
+        h = 0
+    if m_match:
+        m = int(m_match.group(1))
+    else:
+        m = 0
+    if s_match:
+        s = int(s_match.group(1))
+    else:
+        s = 0
+    return hms_to_timestamp(h, m, s)
 
 
 if __name__ == '__main__':
